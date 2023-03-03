fed8818c5c9fffaf9b51a64b3f64d4e47d2b413c
Harry-THP <harryspencer01983@gmail.com>
2023-03-03 15:34:38 +0000

    Updated GUI of Main Menu

--- lib/main_menu.dart
+++ lib/main_menu.dart
@@ -5,106 +5,64 @@ import 'package:tap_tennis/play.dart' as play_page;
 import 'package:tap_tennis/options.dart' as options_page;
 
 class MainMenu extends StatelessWidget {
+  final ButtonStyle btn_Style = ButtonStyle(
+      backgroundColor: MaterialStateProperty.all<Color>(
+          const Color.fromARGB(255, 94, 215, 12)),
+      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+      textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
+          fontSize: 24, fontWeight: FontWeight.bold, fontFamily: "Monospace")),
+      shape: MaterialStateProperty.all<OutlinedBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ));
+
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Menu"),
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            //Play Button
-            Padding(
-              padding: EdgeInsets.only(
-                  top: height / 15, left: height / 100, right: height / 100),
-              child: Container(
-                height: height / 10,
-                width: width / 1.33,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: colours.Palette.playButtonGreen,
-                      foregroundColor: colours.Palette.snow,
-                      textStyle: const TextStyle(fontSize: 45),
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => play_page.main()),
-                    );
-                  },
-                  child: const Text(
-                    "Play",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontFamily: "AdamCGPro"),
-                  ),
-                ),
-              ),
+            const Text(
+              "Tap Tennis",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
-
-            //Settings Button
-            Padding(
-              padding: EdgeInsets.only(
-                  top: height / 100, left: height / 100, right: height / 100),
-              child: Container(
-                height: height / 10,
-                width: width / 1.33,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: colours.Palette.aboutButtonBlue,
-                      foregroundColor: colours.Palette.snow,
-                      textStyle: const TextStyle(fontSize: 45),
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => options_page.Options()),
-                    );
-                  },
-                  child: const Text(
-                    "Options",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontFamily: "AdamCGPro"),
-                  ),
-                ),
-              ),
-            ),
-
-            //Quit Game
-//Play Button
-            Padding(
-              padding: EdgeInsets.only(
-                  top: height / 100, left: height / 100, right: height / 100),
-              child: Container(
-                height: height / 10,
-                width: width / 1.33,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: colours.Palette.quitButtonRed,
-                      foregroundColor: colours.Palette.snow,
-                      textStyle: const TextStyle(fontSize: 45),
-                      shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero)),
+            ElevatedButton(
+                style: btn_Style,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => play_page.main()),
+                  );
+                },
+                child: const Text("Start Game")),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    style: btn_Style,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => options_page.Options()),
+                      );
+                    },
+                    child: const Text("Options")),
+                ElevatedButton(
+                  style: btn_Style,
                   onPressed: () {
                     SystemNavigator.pop();
                   },
-                  child: const Text(
-                    "Quit",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontFamily: "AdamCGPro"),
-                  ),
-                ),
-              ),
-            ),
+                  child: const Text("Quit"),
+                )
+              ],
+            )
           ],
         ),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
